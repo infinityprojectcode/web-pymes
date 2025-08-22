f2506eca17df869f01eed2763331b4a50a8676bc
Deymer Ospina <deymer.ospina@gmail.com>
2025-08-22 00:25:01 -0500

    Initial version finished

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -23,7 +24,15 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 ])
---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+import AppRouter from "@routes/router/root.jsx";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+}
